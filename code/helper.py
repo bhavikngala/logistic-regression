474af63ad2c59b8caa0b976236e6d2a72341e277
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-12 18:40:55 -0500

    kmeans on input data - return centroids and labels

--- code/helper.py
+++ code/helper.py
@@ -1,7 +1,14 @@
 from tensorflow.examples.tutorials.mnist import input_data
+from scipy.cluster.vq import kmeans2
 
 def readMNISTData():
 	mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 	return [mnist.train.images, mnist.train.labels,
 			mnist.validation.images, mnist.validation.labels,
 			mnist.test.images, mnist.test.labels]
+
+# compute cluster centers and labels using kmeans
+def applyKmeans2(data, numClusters):
+	centroids, labels = kmeans2(data, numClusters, 
+		iter=20, minit='points', missing='warn')
+	return [centroids, labels]
