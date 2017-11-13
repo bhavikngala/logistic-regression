0b7bc42410819b7b4b6df98147af1f5c3b09b1a9
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-13 15:32:50 -0500

    helper function for images

---
+++ code/image_helper.py
@@ -0,0 +1,13 @@
+from scipy import misc
+import matplotlib.pyplot as plt
+
+def readImage(filename):
+	img = misc.imread(filaname)
+	return img
+
+def resizeImage():
+	return resizedImaged
+
+def showImage(img):
+	plt.imshow(img)
+	plt.show()
