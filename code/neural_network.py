38f86f8395e5ad39d40ead7f7e7c975f8dd1e61b
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-15 13:20:00 -0500

    stochastic gradient descent training for the model

--- code/neural_network.py
+++ code/neural_network.py
@@ -9,14 +9,6 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 import tensorflow as tf
 
-# create interaci=tive session
-# session = tf.InteractiveSession()
-
-# we build ccomputation graphs before starting the sessions
-# sessions are connection to the backend that run the graphs
-# interactive session allows to be flexible, build stuff on the go
-# otherwise entire graph has to be pre built before launching session
-
 # tensor of any number of rows and 784 columns
 x = tf.placeholder(tf.float32, [None, 784])
 
@@ -24,5 +16,34 @@ x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 
-# softmax model 
+# softmax model, predicted labels 
 y = tf.nn.softmax(tf.matmul(x, W) + b)
+# correct label
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+# cross entropy function
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y),
+	reduction_indices=[1]))
+
+# train backprop with gradient descent
+train_step = tf.train.GradientDescentOptimizer(0.5)\
+	.minimize(cross_entropy)
+
+# create interaci=tive session
+sess = tf.InteractiveSession()
+
+# we build ccomputation graphs before starting the sessions
+# sessions are connection to the backend that run the graphs
+# interactive session allows to be flexible, build stuff on the go
+# otherwise entire graph has to be pre built before launching session
+
+# operation to initialize the variables we created
+tf.global_variables_initializer().run()
+
+# run training step 1000 times
+for _ in range(1000):
+	# batch of random 100 images
+	batch_xs, batch_ys = mnist.train.next_batch(100)
+	# feed the batch in placeholder variables
+	# this is stochastic training
+	sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
