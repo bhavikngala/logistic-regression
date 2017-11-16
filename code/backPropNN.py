bd667290c1881fc6c9d00ec2d9119829f0ff0d5b
Bhavik N Gala <bhavikna@buffalo.edu>
2017-11-16 12:53:07 -0500

    implemented constructor

--- code/backPropNN.py
+++ code/backPropNN.py
@@ -1,11 +1,31 @@
-#  http://neuralnetworksanddeeplearning.com/chap2.html
+import numpy as np
+
+# Reference:
+# http://neuralnetworksanddeeplearning.com/chap2.html
 
 class backPropNN:
 
 	# class instantiation/constructor
-	# 
-	def __init__(self, ):
-		return None
+	def __init__(self, layerSizes, learningRate, l2Lambda):
+		# number of layers in the network
+		self.numLayers = len(layerSizes)
+		# number of neurons in each layer
+		self.layerSizes = layerSizes
+		# learning rate for SGD
+		self.learningRate = learningRate
+		# growth decay regularizer
+		self.l2Lambda = l2Lambda
+
+		# initialize weights and biases for each layer
+		# weights for 1st layer are always 1
+		# biases for 1st layer are always 0
+		self.weights = [np.ones([layerSizes[0], 1])]
+		self.biases = [np.zeros([layerSizes[0], 1])]
+				
+		for i in range(1, len(layerSizes)):
+			self.weights.append(np.zeros([layerSizes[i],
+				layerSizes[i-1]]))
+			self.biases.append(np.zeros([layerSize[i], 1]))
 
 	# for each l=2..L compute
 	# 1. zl=wlal−1+bl
